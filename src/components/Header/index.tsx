4cb38e8320a9bf3822418bf49da44e1f47165c4e
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-13 15:00:51 -0300

    update Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import Switch from "react-switch";
+import { ThemeContext } from "styled-components";
+import { shade } from "polished";
 
 import { Container } from "./styles";
 
-const Header: React.FC = () => <Container>Hello Word</Container>;
+interface Props {
+  toggleTheme(): void;
+}
+
+const Header: React.FC<Props> = ({ toggleTheme }) => {
+  const { colors, title } = useContext(ThemeContext);
+
+  return (
+    <Container>
+      Hello Word
+      <Switch
+        onChange={toggleTheme}
+        checked={title === "dark"}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        height={10}
+        width={40}
+        handleDiameter={20}
+        offColor={shade(0.15, colors.primary)}
+        onColor={colors.secundary}
+      />
+    </Container>
+  );
+};
 
 export default Header;
