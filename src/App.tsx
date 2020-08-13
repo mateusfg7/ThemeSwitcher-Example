cfe10a4ef2c8bcaba0ba99a1f3e54df9c89b4cef
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-13 15:01:12 -0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import usePersistedState from "./utils/usePersistedState";
+
+import light from "./styles/themes/light";
+import dark from "./styles/themes/dark";
 
 import GlobalStyles from "./styles/global";
 import Header from "./components/Header";
 
-function App() {
+const App = () => {
+  const [theme, setTheme] = usePersistedState("theme", light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === "light" ? dark : light);
+  };
+
   return (
-    <div className='App'>
-      <GlobalStyles />
-      <Header />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <GlobalStyles />
+        <Header toggleTheme={toggleTheme} />
+      </div>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
